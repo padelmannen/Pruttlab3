039d7a8265ec4ba3dc8623fbfc75ce0c750128ae
padelmannen <jesper.senke@gmail.com>
2021-11-15 14:46:55 +0100

    jobbar på skel

--- LAB3/src/SSP/RPSSkel.java
+++ LAB3/src/SSP/RPSSkel.java
@@ -16,18 +16,28 @@ class RPSSkel extends JFrame {
     JButton closebutton;
 
     RPSSkel () {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        closebutton = new JButton("Close");
-        myboard = new Gameboard("Myself"); // Must be changed
-        computersboard = new Gameboard("Computer");
-        JPanel boards = new JPanel();
-        boards.setLayout(new GridLayout(1,2));
-        boards.add(myboard);
-        boards.add(computersboard);
-        add(boards, BorderLayout.CENTER);
-        add(closebutton, BorderLayout.SOUTH);
-        setSize(350, 650);
-        setVisible(true);
+        try {
+            socket = new Socket("localhost", 4713);
+            in=new BufferedReader
+                    (new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            closebutton = new JButton("Close");
+            myboard = new Gameboard("Myself"); // Must be changed
+            computersboard = new Gameboard("Computer");
+            JPanel boards = new JPanel();
+            boards.setLayout(new GridLayout(1, 2));
+            boards.add(myboard);
+            boards.add(computersboard);
+            add(boards, BorderLayout.CENTER);
+            add(closebutton, BorderLayout.SOUTH);
+            setSize(350, 650);
+            setVisible(true);
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
     }
 
     public static void main (String[] u) {
